3ea55bd976e00593cea4fd5a6dd79a2edd220ecd
NOTE111 <lolly5488@gmail.com>
2023-04-28 00:33:28 +0900

    [BE] Fix : 모든 오리진 허용

--- backend/src/main/java/backend/com/backend/config/WebConfig.java
+++ backend/src/main/java/backend/com/backend/config/WebConfig.java
@@ -11,7 +11,24 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
                 .allowedMethods("POST", "GET", "PATCH", "DELETE", "OPTIONS")
                 .maxAge(3600);
     }
 }
+
+/*@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://example.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}*/
